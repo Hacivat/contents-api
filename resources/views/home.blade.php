8be382067f4a4f40f014eb6538ac31d65f75d0aa
Osman Pekaydın <pekaydin580@gmail.com>
2020-06-21 17:36:03 +0300

    updated insert form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,31 +14,32 @@
                         </div>
                     @endif
 
-                    <form>
-                        <select class="form-control form-control-sm">
+                    <form id="insert" action="{{ action('ContentController@store') }}">
+                        <input name="_token" type="hidden" value="{{ csrf_token() }}"/>
+                        <select class="form-control form-control-sm" name="category">
                             @foreach($categories as $value) 
-                            <option value="{{ $value->id }}">{{ $value->category_tr }}</option>
+                                <option value="{{ $value->category_en }}">{{ $value->category_tr }}</option>
                             @endforeach
                         </select>
-                        <div class="custom-control custom-radio">
-                            <input type="radio" id="customRadio1" name="customRadio" class="custom-control-input">
-                            <label class="custom-control-label" for="customRadio1">TR</label>
+                        <div class="custom-control custom-radio mt-2">
+                            <input type="radio" id="radio_tr" value="tr"  name="lang" class="custom-control-input">
+                            <label class="custom-control-label" for="radio_tr">TR</label>
                         </div>
                         <div class="custom-control custom-radio">
-                            <input type="radio" id="customRadio2" name="customRadio" class="custom-control-input">
-                            <label class="custom-control-label" for="customRadio2">EN</label>
+                            <input type="radio" id="radio_en" value="en"  name="lang" class="custom-control-input">
+                            <label class="custom-control-label" for="radio_en">EN</label>
                         </div>
-                        <div class="custom-control custom-radio">
-                            <input type="radio" id="customRadio2" name="customRadio" class="custom-control-input">
-                            <label class="custom-control-label" for="customRadio2">EN</label>
+                        <div class="form-group mt-2">
+                            <label for="input_title">Title</label>
+                            <input class="form-control" name="title" id="input_title" type="text">
                         </div>
-                        <div class="form-group  mt-2">
-                            <label for="exampleFormControlTextarea1">Content</label>
-                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        <div class="form-group mt-2">
+                            <label for="textarea_content">Content</label>
+                            <textarea class="form-control" name="content" id="textarea_content" rows="3"></textarea>
                         </div>
                         <div class="form-group row">
-                        <div class="col-sm-10">
-                            <button type="submit" class="btn btn-primary">Add</button>
+                            <div class="col-sm-10">
+                                <button type="submit" id="submit" class="btn btn-primary">Add</button>
                             </div>
                         </div>
                     </form>
@@ -48,3 +49,24 @@
     </div>
 </div>
 @endsection
+
+<script src="{{ asset('js/jquery-3.5.1.min.js') }}"></script>
+<script>
+
+$( document ).ready(function() {
+    $( "#submit" ).click(function( event ) {
+        event.preventDefault();
+
+        $( "#insert" ).submit();
+
+        // var values = {};
+        // $.each($('#insert').serializeArray(), function(i, field) {
+        //     values[field.name] = field.value;
+        // });
+        // console.log('values', values)
+    });
+});
+
+</script>
+
+
