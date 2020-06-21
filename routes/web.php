b8fb795b68e6bcea81b252ba07974f7f0dfda194
Osman Pekaydın <pekaydin580@gmail.com>
2020-06-21 20:45:23 +0300

    added web and api routes

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,5 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/categories', 'CategoryController@getAll');
+Route::post('/add', 'ContentController@store')->name('add');
 
