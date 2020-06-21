253aab4cf42ae8810d6a02b7e0eec9f2e63c508b
Osman Pekaydın <pekaydin580@gmail.com>
2020-06-21 03:41:44 +0300

    Added functions to talecontroller

--- app/Http/Controllers/TaleController.php
+++ app/Http/Controllers/TaleController.php
@@ -3,8 +3,50 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use App\Tale;
 
 class TaleController extends Controller
 {
-    //
+    public function store (Request $request) {
+        $tale = new Tale();
+
+        if ($lang == 'en') {
+            $tale->text_en = $request->text;
+            $tale->title_en = $request->title;
+        }
+        else {
+            $tale->text_tr = $request->text;
+            $tale->title_tr = $request->title;
+        } 
+    
+        $tale->uuid = Str::uuid()->toString();
+        $tale->isActive = 1;
+        if ($tale->save()) 
+            return response()->json([
+                'status_code' => 200,
+                'message' => 'Created'
+            ]);
+        else 
+            return response()->json([
+                'status_code' => 400,
+                'message' => 'Failed'
+            ]);
+    }
+
+    public function getTitles () {
+        //
+    }
+
+    public function getContent ($lang = 'tr') {
+        //
+    }
+
+    public function getImages () {
+        //
+    }
+
+    public function getVoice ($lang = 'tr') {
+        //
+    }
 }
