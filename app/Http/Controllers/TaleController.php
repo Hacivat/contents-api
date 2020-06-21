f339074e4bda0e1c6ac154b6da79a066b0f705dc
Osman Pekaydın <pekaydin580@gmail.com>
2020-06-22 00:46:12 +0300

    added statements to api endpoints

--- app/Http/Controllers/TaleController.php
+++ app/Http/Controllers/TaleController.php
@@ -11,13 +11,25 @@ class TaleController extends Controller
     public function getTitles ($lang = 'tr') {
         $tales = Tale::get();
         if ($lang === 'en')
-            return response()->json($tales->pluck('title_en'), JSON_UNESCAPED_UNICODE );
+            return response()->json($tales->pluck('title_en'), JSON_UNESCAPED_UNICODE);
         else 
             return response()->json($tales->pluck('title_tr'), JSON_UNESCAPED_UNICODE);
     }
 
-    public function getContent ($lang = 'tr') {
+    public function getTitlesAndImages ($lang = 'tr') {
         //
+    }
+
+    public function getContent ($uuid, $lang = 'tr') {
+        $tale = Tale::where('uuid', $uuid)->get();
+
+        if ($lang === 'en') {
+            $tale->makeHidden(['title_tr','text_tr']);
+            return response()->json($tale, JSON_UNESCAPED_UNICODE);
+        } else {
+            $tale->makeHidden(['title_en','text_en']);
+            return response()->json($tale, JSON_UNESCAPED_UNICODE);
+        }
     }
 
     public function getImages () {
