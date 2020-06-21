cd7690451318336dbb885507457c59990626e707
Osman Pekaydın <pekaydin580@gmail.com>
2020-06-21 03:13:38 +0300

    Updated content insert page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,34 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <form>
+                        <select class="form-control form-control-sm">
+                            @foreach($categories as $value) 
+                            <option value="{{ $value->id }}">{{ $value->category_tr }}</option>
+                            @endforeach
+                        </select>
+                        <div class="custom-control custom-radio">
+                            <input type="radio" id="customRadio1" name="customRadio" class="custom-control-input">
+                            <label class="custom-control-label" for="customRadio1">TR</label>
+                        </div>
+                        <div class="custom-control custom-radio">
+                            <input type="radio" id="customRadio2" name="customRadio" class="custom-control-input">
+                            <label class="custom-control-label" for="customRadio2">EN</label>
+                        </div>
+                        <div class="custom-control custom-radio">
+                            <input type="radio" id="customRadio2" name="customRadio" class="custom-control-input">
+                            <label class="custom-control-label" for="customRadio2">EN</label>
+                        </div>
+                        <div class="form-group  mt-2">
+                            <label for="exampleFormControlTextarea1">Content</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div>
+                        <div class="form-group row">
+                        <div class="col-sm-10">
+                            <button type="submit" class="btn btn-primary">Add</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
